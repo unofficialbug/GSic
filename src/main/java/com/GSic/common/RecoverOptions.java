cb4477a0a88c5f5b67f501d9eb51f1d9ecb22ce0
unofficialbug <maedehdehghanam@gmail.com>
2021-07-06 21:44:48 +0430

    add recoveroptions

---
+++ src/main/java/com/GSic/common/RecoverOptions.java
@@ -0,0 +1,11 @@
+package com.GSic.common;
+import java.io.*;
+import java.util.*;
+//recovering account by answering thses questions
+public enum RecoverOptions implements Serializable{
+	DREAM_JOB,
+	MOM_BIRTHDAY,
+	BEST_VACATION,
+	CHILDHOOD_HERO,
+	FIRST_TEACHER
+}
